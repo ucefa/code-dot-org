7f9cec4fa288e5c8d4824fe7d3d99cd752f53d21
Brent Van Minnen <bjvanminnen@gmail.com>
2017-04-17 17:01:32 -0700

    ES6ify some JS related to markdown

--- apps/src/code-studio/initializeEmbeddedMarkdownEditor.js
+++ apps/src/code-studio/initializeEmbeddedMarkdownEditor.js
@@ -2,8 +2,8 @@
  * @file Defines a function for initializing an embedded markdown editor using
  *       CodeMirror and marked.
  */
-var marked = require("marked");
-var initializeCodeMirror = require("./initializeCodeMirror");
+import marked from 'marked';
+import initializeCodeMirror from './initializeCodeMirror';
 
 marked.setOptions({
   sanitize: true
@@ -21,34 +21,33 @@ marked.setOptions({
  *
  * For a non DSL element, use initializeCodeMirror
  *
- * @param {jQuery} embeddedElement textarea element within which to embed the markdown
- * @param {string} markdownTextArea id (which will be prefixed by "level_")
+ * @param {jQuery} embeddedElement - textarea element within which to embed the markdown
+ * @param {string} markdownTextArea - id (which will be prefixed by "level_")
  *                                  of textarea where editor will live
- * @param {string} name of the property within the textarea
+ * @param {string} markdownProperty - name of the property within the textarea
  */
-module.exports = function (embeddedElement, markdownTextArea, markdownProperty) {
-  var regex = new RegExp("^" + markdownProperty + " <<(\\w*)\\n([\\s\\S]*?)\\n\\1\\s*$", "m");
-  var dslElement = embeddedElement;
-  var dslText = dslElement.val();
+export default function initializeEmbeddedMarkdownEditor(embeddedElement,
+    markdownTextArea, markdownProperty) {
+  const regex = new RegExp("^" + markdownProperty + " <<(\\w*)\\n([\\s\\S]*?)\\n\\1\\s*$", "m");
+  const dslText = embeddedElement.val();
 
-  var mdEditor = initializeCodeMirror(markdownTextArea, 'markdown', function (editor, change) {
-    var editorText = editor.getValue();
-    var dslText = dslElement.val();
-    var replacedText;
+  const mdEditor = initializeCodeMirror(markdownTextArea, 'markdown', (editor, change) => {
+    const editorText = editor.getValue();
+    const dslText = embeddedElement.val();
+    let replacedText;
     if (regex.exec(dslText)) {
       replacedText = dslText.replace(regex, markdownProperty + ' <<$1\n' + editorText + '\n$1\n');
     } else {
       replacedText = dslText + '\n' + markdownProperty + ' <<MARKDOWN\n' + editorText + '\nMARKDOWN\n';
     }
-    dslElement.val(replacedText);
+    embeddedElement.val(replacedText);
   }, true);
 
   // Match against markdown heredoc syntax and capture contents in [2].
-  var match = regex.exec(dslText);
+  const match = regex.exec(dslText);
   if (match && match[2]) {
-    var markdownText = match[2];
-    mdEditor.setValue(markdownText);
+    mdEditor.setValue(match[2]);
   } else {
     mdEditor.setValue('');
   }
-};
+}
